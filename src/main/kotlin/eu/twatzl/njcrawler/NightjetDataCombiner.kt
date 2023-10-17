789bfdec884b4217969b67b7f53c84e906bd09ea
Torben Woltjen <7781282-mozzbozz@users.noreply.gitlab.com>
2023-10-17 22:14:56 +0200

    remove missing import

--- src/main/kotlin/eu/twatzl/njcrawler/NightjetDataCombiner.kt
+++ src/main/kotlin/eu/twatzl/njcrawler/NightjetDataCombiner.kt
@@ -4,8 +4,6 @@ import eu.twatzl.njcrawler.service.NightjetPersistenceService
 import eu.twatzl.njcrawler.util.getCurrentTime
 import eu.twatzl.njcrawler.util.getFormattedDate
 import java.nio.file.Paths
-import kotlin.io.path.Path
-
 
 /**
  * reads multiple connection csv files and combines them into a single csv file
