12c8154fd76a45d281c25a518f46db9b38e8b9a7
Martin Langbecker <Martin.Langbecker@gmail.com>
2024-01-02 22:24:34 +0100

    simplify date condition

--- src/main/kotlin/eu/twatzl/njcrawler/data/EuropeanSleeperData.kt
+++ src/main/kotlin/eu/twatzl/njcrawler/data/EuropeanSleeperData.kt
@@ -2,10 +2,10 @@ package eu.twatzl.njcrawler.data
 
 import eu.twatzl.njcrawler.model.Station
 import eu.twatzl.njcrawler.model.TrainConnection
-import eu.twatzl.njcrawler.util.getCurrentTime
-import eu.twatzl.njcrawler.util.getTimezone
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.toLocalDateTime
+import eu.twatzl.njcrawler.util.getCurrentDay
+import kotlinx.datetime.LocalDate
+
+val today = getCurrentDay()
 
 // station data can be retrieved by opening https://www.europeansleeper.eu/
 // and inspecting the source code of the departure or arrival station input field
@@ -16,11 +16,11 @@ val esPraha = Station("Prag hl.n.", "5457076")
 
 // TODO remove conditional assignments some time after March 25/26, 2024
 
-var es452 = if (getCurrentTime().toLocalDateTime(getTimezone()) < LocalDateTime(2024, 3, 26, 0, 0, 0))
+var es452 = if (today <= LocalDate(2024, 3, 24))
     TrainConnection("ES 452", esBerlinOst, esBruxelles) // until March 24, 2024
 else TrainConnection("ES 452", esPraha, esBruxelles) // starting from March 26, 2024
 
-val es453 = if (getCurrentTime().toLocalDateTime(getTimezone()) < LocalDateTime(2024, 3, 25, 0, 0, 0))
+val es453 = if (today <= LocalDate(2024, 3, 22))
     TrainConnection("ES 453", esBruxelles, esBerlinOst) // until March 22, 2024
 else TrainConnection("ES 453", esBruxelles, esPraha) // starting from March 25, 2024
 
--- src/main/kotlin/eu/twatzl/njcrawler/util/util.kt
+++ src/main/kotlin/eu/twatzl/njcrawler/util/util.kt
@@ -6,6 +6,8 @@ fun getTimezone() = TimeZone.currentSystemDefault()
 
 fun getCurrentTime() = Clock.System.now()
 
+fun getCurrentDay() = getCurrentTime().toLocalDateTime(getTimezone()).date
+
 fun getFormattedTime(i: Instant): String {
     val t = i.toLocalDateTime(getTimezone())
     return "%04d-%02d-%02d_%02d-%02d".format(t.year, t.monthNumber, t.dayOfMonth, t.hour, t.minute)
